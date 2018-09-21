7174f52a9e09660e7afbbc07ab5937742e5260ad
Sander Soots <sander.soots@eesti.ee>
2018-09-21 22:59:36 +0300

    Add project files.

---
+++ 1oT/Services/ToastNotificationsService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using _1oT.Activation;
+
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Notifications;
+
+namespace _1oT.Services
+{
+    internal partial class ToastNotificationsService : ActivationHandler<ToastNotificationActivatedEventArgs>
+    {
+        public void ShowToastNotification(ToastNotification toastNotification)
+        {
+            ToastNotificationManager.CreateToastNotifier().Show(toastNotification);
+        }
+
+        protected override async Task HandleInternalAsync(ToastNotificationActivatedEventArgs args)
+        {
+            //// TODO WTS: Handle activation from toast notification
+            //// More details at https://docs.microsoft.com/windows/uwp/design/shell/tiles-and-notifications/send-local-toast
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ 1oT/Views/MasterDetailPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+using _1oT.Models;
+using _1oT.Services;
+
+using Microsoft.Toolkit.Uwp.UI.Controls;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace _1oT.Views
+{
+    public sealed partial class MasterDetailPage : Page, INotifyPropertyChanged
+    {
+        private SampleOrder _selected;
+
+        public SampleOrder Selected
+        {
+            get { return _selected; }
+            set { Set(ref _selected, value); }
+        }
+
+        public ObservableCollection<SampleOrder> SampleItems { get; private set; } = new ObservableCollection<SampleOrder>();
+
+        public MasterDetailPage()
+        {
+            InitializeComponent();
+            Loaded += MasterDetailPage_Loaded;
+        }
+
+        private async void MasterDetailPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            SampleItems.Clear();
+
+            var data = await SampleDataService.GetSampleModelDataAsync();
+
+            foreach (var item in data)
+            {
+                SampleItems.Add(item);
+            }
+
+            if (MasterDetailsViewControl.ViewState == MasterDetailsViewState.Both)
+            {
+                Selected = SampleItems.First();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
